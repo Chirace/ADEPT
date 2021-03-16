835f62ddb0f3fbe1d2aa5c6a3009e075a6f72a5d
Chirace <alexis.chirat@etud.u-picardie.fr>
2021-03-16 15:38:20 +0100

    Page d'authentification

--- src/Controller/GeneralController.php
+++ src/Controller/GeneralController.php
@@ -1,9 +1,9 @@
 <?php 
 namespace App\Controller; 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class GeneralController extends Controller {
+class GeneralController extends AbstractController {
 
     /**
      * @Route("/", name="home")
