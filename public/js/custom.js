4363fcb9b503784c66829b888bac2a1db042bb33
Chirace <alexis.chirat@etud.u-picardie.fr>
2021-05-07 14:47:34 +0200

    Avancement navigation et import JQuery et JS

---
+++ public/js/custom.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+});
--- src/Controller/GeneralController.php
+++ src/Controller/GeneralController.php
@@ -1,12 +1,26 @@
 <?php 
 namespace App\Controller; 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class GeneralController extends AbstractController {
 
     public function analyzeChoice(){
         return $this->render('general/analyseChoice.html.twig');
+    }
+
+    /**
+     * @Route("/change_locale/{locale}", name="change_locale")
+     */
+    public function changeLocale($locale, Request $request)
+    {
+        // On stocke la langue dans la session
+        $request->getSession()->set('_locale', $locale);
+
+        // On revient sur la page précédente
+        return $this->redirect($request->headers->get('referer'));
     }
 
     public function home(){
@@ -33,7 +47,7 @@ class GeneralController extends AbstractController {
         return $this->render('general/settings.html.twig');
     }
 
-    public function survey(){
-        return $this->render('general/survey.html.twig');
+    public function guide(){
+        return $this->render('general/guide.html.twig');
     }
 }
